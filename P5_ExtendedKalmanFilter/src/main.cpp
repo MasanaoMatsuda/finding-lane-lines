e5b35ca1ce353da43cb1b9b0a7511353c4e85c93
MasanaoMatsuda <m.masa7206@gmail.com>
2019-08-26 18:48:04 +0900

    debug

--- P5_ExtendedKalmanFilter/src/main.cpp
+++ P5_ExtendedKalmanFilter/src/main.cpp
@@ -45,119 +45,121 @@ int main()
     // pass pointers as arguments into the header of h.onMessage() function
     h.onMessage([&fusionEKF, &tools, &estimations, &ground_truth]
                 (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
-                {
-                // "42" at the start of the message means there's a websocket message event.
-                // The 4 signifies a websocket message.
-                // The 2 signifies a websocket event.
-                if (length && length > 2 && data[0] == '4' && data[1] == '2')
-                {
-                    auto s = hasData(string(data));
+    {
+        // "42" at the start of the message means there's a websocket message event.
+        // The 4 signifies a websocket message.
+        // The 2 signifies a websocket event.
+        cout << "onMessage()" << endl;
 
-                    if (s != "")
+        if (length && length > 2 && data[0] == '4' && data[1] == '2')
+        {
+            auto s = hasData(string(data));
+
+            if (s != "")
+            {
+                auto j = json::parse(s);
+                string event = j[0].get<string>();
+
+                if (event == "telemetry")
+                {
+                    // j[1] is the data JSON object
+                    string sensor_measurement = j[1]["sensor_measurement"];
+
+                    MeasurementPackage meas_package;
+                    std::istringstream iss(sensor_measurement);
+
+                    long long timestamp;
+
+                    string sensor_type;
+                    iss >> sensor_type;
+
+                    if (sensor_type.compare("L") == 0)
                     {
-                        auto j = json::parse(s);
-                        string event = j[0].get<string>();
+                        meas_package.sensor_type_ = MeasurementPackage::LASER;
+                        meas_package.raw_measurements_ = VectorXd(2);
+                        float px;
+                        float py;
+                        iss >> px;
+                        iss >> py;
+                        meas_package.raw_measurements_ << px, py;
 
-                        if (event == "telemetry")
-                        {
-                            // j[1] is the data JSON object
-                            string sensor_measurement = j[1]["sensor_measurement"];
+                        iss >> timestamp;
+                        meas_package.timestamp_ = timestamp;
+                    }
+                    else if (sensor_type.compare("R") == 0)
+                    {
+                        meas_package.sensor_type_ = MeasurementPackage::RADAR;
+                        meas_package.raw_measurements_ = VectorXd(3);
+                        float ro;
+                        float phi;
+                        float ro_dot;
+                        iss >> ro;
+                        iss >> phi;
+                        iss >> ro_dot;
+                        meas_package.raw_measurements_ << ro, phi, ro_dot;
 
-                            MeasurementPackage meas_package;
-                            std::istringstream iss(sensor_measurement);
+                        iss >> timestamp;
+                        meas_package.timestamp_ = timestamp;
+                    }
 
-                            long long timestamp;
+                    float x_gt;
+                    float y_gt;
+                    float vx_gt;
+                    float vy_gt;
+                    iss >> x_gt;
+                    iss >> y_gt;
+                    iss >> vx_gt;
+                    iss >> vy_gt;
 
-                            string sensor_type;
-                            iss >> sensor_type;
+                    VectorXd gt_values(4);
+                    gt_values(0) = x_gt;
+                    gt_values(1) = y_gt;
+                    gt_values(2) = vx_gt;
+                    gt_values(3) = vy_gt;
+                    ground_truth.push_back(gt_values);
 
-                            if (sensor_type.compare("L") == 0)
-                            {
-                                meas_package.sensor_type_ = MeasurementPackage::LASER;
-                                meas_package.raw_measurements_ = VectorXd(2);
-                                float px;
-                                float py;
-                                iss >> px;
-                                iss >> py;
-                                meas_package.raw_measurements_ << px, py;
+                    fusionEKF.ProcessMeasurement(meas_package);
 
-                                iss >> timestamp;
-                                meas_package.timestamp_ = timestamp;
-                            }
-                            else if (sensor_type.compare("R") == 0)
-                            {
-                                meas_package.sensor_type_ = MeasurementPackage::RADAR;
-                                meas_package.raw_measurements_ = VectorXd(3);
-                                float ro;
-                                float phi;
-                                float ro_dot;
-                                iss >> ro;
-                                iss >> phi;
-                                iss >> ro_dot;
-                                meas_package.raw_measurements_ << ro, phi, ro_dot;
+                    VectorXd estimate(4);
+                    double p_x = fusionEKF.ekf_.x_(0);
+                    double p_y = fusionEKF.ekf_.x_(1);
+                    double v1 = fusionEKF.ekf_.x_(2);
+                    double v2 = fusionEKF.ekf_.x_(3);
+                    estimate(0) = p_x;
+                    estimate(1) = p_y;
+                    estimate(2) = v1;
+                    estimate(3) = v2;
+                    estimations.push_back(estimate);
 
-                                iss >> timestamp;
-                                meas_package.timestamp_ = timestamp;
-                            }
+                    VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
 
-                            float x_gt;
-                            float y_gt;
-                            float vx_gt;
-                            float vy_gt;
-                            iss >> x_gt;
-                            iss >> y_gt;
-                            iss >> vx_gt;
-                            iss >> vy_gt;
-
-                            VectorXd gt_values(4);
-                            gt_values(0) = x_gt;
-                            gt_values(1) = y_gt;
-                            gt_values(2) = vx_gt;
-                            gt_values(3) = vy_gt;
-                            ground_truth.push_back(gt_values);
-
-                            fusionEKF.ProcessMeasurement(meas_package);
-
-                            VectorXd estimate(4);
-                            double p_x = fusionEKF.ekf_.x_(0);
-                            double p_y = fusionEKF.ekf_.x_(1);
-                            double v1 = fusionEKF.ekf_.x_(2);
-                            double v2 = fusionEKF.ekf_.x_(3);
-                            estimate(0) = p_x;
-                            estimate(1) = p_y;
-                            estimate(2) = v1;
-                            estimate(3) = v2;
-                            estimations.push_back(estimate);
-
-                            VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
-
-                            json msgJson;
-                            msgJson["estimate_x"] = p_x;
-                            msgJson["estimate_y"] = p_y;
-                            msgJson["rmse_x"] = RMSE(0);
-                            msgJson["rmse_y"] = RMSE(1);
-                            msgJson["rmse_vx"] = RMSE(2);
-                            msgJson["rmse_vy"] = RMSE(3);
-                            auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-                            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-                        }
-                        else
-                        {
-                            string msg = "42[\"manual\", {}]";
-                            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-                        }
-                    } // end websocket message if
-                }}); // end h.onMessage
+                    json msgJson;
+                    msgJson["estimate_x"] = p_x;
+                    msgJson["estimate_y"] = p_y;
+                    msgJson["rmse_x"] = RMSE(0);
+                    msgJson["rmse_y"] = RMSE(1);
+                    msgJson["rmse_vx"] = RMSE(2);
+                    msgJson["rmse_vy"] = RMSE(3);
+                    auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
+                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                }
+                else
+                {
+                    string msg = "42[\"manual\", {}]";
+                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                }
+            } // end websocket message if
+        }}); // end h.onMessage
         h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req)
-                       {
-                       std::cout << "Connected!!!" << std::endl;
-                       });
+        {
+            std::cout << "Connected!!!" << std::endl;
+        });
 
         h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length)
-                          {
-                          ws.close();
-                          std::cout << "Disconnected" << std::endl;
-                          });
+        {
+            ws.close();
+            std::cout << "Disconnected" << std::endl;
+        });
 
         int port = 4567;
         if (h.listen(port))
