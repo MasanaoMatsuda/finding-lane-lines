de9209377d6e17de59d9bbf4cc462f1fa049eedb
MasanaoMatsuda <m.masa7206@gmail.com>
2019-08-26 17:21:07 +0900

    modify error

--- P5_ExtendedKalmanFilter/src/kalman_filter.cpp
+++ P5_ExtendedKalmanFilter/src/kalman_filter.cpp
@@ -4,12 +4,12 @@ KalmanFilter::KalmanFilter()
 {
 }
 
-KalmanFilter::~KalmanFilter();
+KalmanFilter::~KalmanFilter()
 {
 }
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in);
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in)
 {
     x_ = x_in;
     P_ = P_in;
@@ -22,17 +22,17 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 void KalmanFilter::Predict()
 {
     x_ = F_ * x_;
-    P_ = F_ * P_ * F_.transpose() + Q;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z)
 {
-    y = z - H_ * x_;
-    S = H_ * P_ * H_.transpose() + R_;
-    K = P_ * H_.transpose() * S.inverse();
+    VecrotXd y = z - H_ * x_;
+    MatrixXd S = H_ * P_ * H_.transpose() + R_;
+    MatrixXd K = P_ * H_.transpose() * S.inverse();
 
     x_ = x_ + (K * y);
-    I_ = MatrixXd::Identity(x_.size(), x_.size());
+    MatrixXd I_ = MatrixXd::Identity(x_.size(), x_.size());
     P_ = (I_ - K * H_) * P_;
 }
 
